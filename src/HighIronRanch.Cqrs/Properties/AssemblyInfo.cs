aef527339405de58957bc1b6cc27ac71b9df3d30
Stephen Balkum <stephen@balkum.com>
2015-04-21 15:21:22 -0500

    Updated dependencies. Added nuget package build step.

--- src/HighIronRanch.Cqrs/Properties/AssemblyInfo.cs
+++ src/HighIronRanch.Cqrs/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HighIronRanch.Cqrs")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An generic implementation of SimpleCqrsRuntime for github.com/sbalkum/SimpleCQRS, a fork of github.com/tyronegroves/SimpleCQRS.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Stephen Balkum")]
 [assembly: AssemblyProduct("HighIronRanch.Cqrs")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2015 Stephen Balkum")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
